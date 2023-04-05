c00428c0250a9af28b355b229f5739297ebebefa
CLENALIO <caroline.le.nalio@gmail.com>
2023-04-05 13:31:05 +0200

    quete 7bis

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
 import NavBar from "./components/NavBar"
+import { useEffect } from "react";
 
 function App() {
   const pokemonList = [
@@ -41,6 +42,13 @@ function App() {
     }
 
     const pokemon = pokemonList[pokemonIndex]
+
+    useEffect(
+      () =>  {
+        alert("hello pokemon trainer :)")
+      }, 
+      []
+   );
 
   return (
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useEffect } from "react";
 
 const NavBar = ({pokemonIndex, pokemonList, handleClick, handleClick2}) => {
+
+
+    useEffect(
+        () =>  {
+            pokemonIndex === 3 ? alert("pika pikachu !!!") : null
+        }, 
+     );
+
     return (
         <div>
         {pokemonIndex > 0 ? <button onClick={handleClick}> Précédent</button> : null}
-        {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClick2}> Suivant</button> : null}        
+        {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClick2}> Suivant</button> : null}  
         </div>
     );
 };
