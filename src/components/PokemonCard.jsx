6669294194f95824da4d35e229f0b2b77e427eef
CLENALIO <caroline.le.nalio@gmail.com>
2023-04-05 11:41:33 +0200

    4eme quete

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-const PokemonCard = () => {
 
-    const pokemon = pokemonList[1]
+const PokemonCard = ({pokemon}) => {
 
     return (
             <figure>
